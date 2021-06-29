be9fe4b2fb4ada20efd11ed677917d1f77914050
Ernaes Trujillo <Ernaes Trujillo@DESKTOP-IBIFLMN>
2021-06-28 19:25:27 -0500

    Agregar archivos de proyecto.

---
+++ TaskIO/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskIO.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string usuario { get; set; }
+        public string contrasena { get; set; }
+        public string nombre { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public string email { get; set; }
+        public DateTime? fechaValidacion { get; set; }
+    }
+}
